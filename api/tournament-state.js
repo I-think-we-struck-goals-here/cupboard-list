20f8f21625f4b62bbbb8155ee36ed765499808a6
I-think-we-struck-goals-here <zacellis+6s@gmail.com>
2026-07-11 13:54:10 +0100

    Apply tournament read caching from the function response

--- api/tournament-state.js
+++ api/tournament-state.js
@@ -1,3 +1,12 @@
 import { handleTournament } from "./_tournament-core.js";
 
-export default handleTournament;
+export default async function tournamentState(req, res) {
+  if (req.method === "GET") {
+    res.setHeader(
+      "Vercel-CDN-Cache-Control",
+      "max-age=15, stale-while-revalidate=2, stale-if-error=60",
+    );
+  }
+
+  return handleTournament(req, res);
+}
